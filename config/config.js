52501f9aae4b6ac1b602b0c55d5870a2cf5620cd
rupsa45 <drupsa767@gmail.com>
2025-07-07 00:40:18 +0530

    update database connection settings

--- config/config.js
+++ config/config.js
@@ -14,10 +14,10 @@ module.exports ={
     "dialect": "mysql"
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
+    "username": process.env.DB_USERNAME,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_NAME_DEV,
+    "host": process.env.DB_HOST,
     "dialect": "mysql"
   }
 }
